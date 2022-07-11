f8944b31bb9744573e69946dd4fcacc2edfce841
Keks <keks@htmlacademy.ru>
2022-07-11 13:06:36 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/photo.js
+++ 6/js/photo.js
@@ -1,9 +1,9 @@
-// 7.15. Отрисуй меня полностью (часть 1)
-
 import { creatRandomPost } from './data.js';
 
+// 7.15. Отрисуй меня полностью (часть 1)
+
 const pictures = document.querySelector('.pictures');
-const template = document.querySelector('#picture').
+const pictureTemplate = document.querySelector('#picture').
   content.
   querySelector('.picture');
 
@@ -12,13 +12,12 @@ const photoData = creatRandomPost();
 const picturesFragment = document.createDocumentFragment();
 
 photoData.forEach(({ url, comments, likes }) => {
-  const pictureElement = template.cloneNode(true);
+  const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__comments').textContent =
     comments.length;
-  pictureElement.querySelector('picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
   picturesFragment.append(pictureElement);
 });
 
 pictures.append(picturesFragment);
-
